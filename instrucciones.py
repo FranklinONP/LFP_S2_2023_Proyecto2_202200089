f722a102b2e4e2806b8d4dbd5645b7ea83562faa
Franklin ONP <3110022770701@ingenieria.usac.edu.gt>
2023-10-08 22:57:31 -0600

    caracters
    Se pueden extraer los lexemas, aun no se pueden caputrar los erroes, no se tiene claro como podrian venir

---
+++ instrucciones.py
@@ -0,0 +1,6 @@
+class instrucciones():
+    def __init__(self,lexemaInstruccion):
+        self.lexemaInstruccion=lexemaInstruccion
+        
+    def getLexemaInstruccion(self):
+        return self.lexemaInstruccion
---
+++ registros.py
@@ -0,0 +1,8 @@
+class instrucciones():
+    def __init__(self,codigo,producto,pcompra,pventa,stock):
+        self.codigo=codigo
+        self.producto=producto
+        self.pcompra=pcompra
+        self.pventa=pventa
+        self.stock=stock
+        
